356e7b564540dd9f6291c5a26e3251d10525ae6f
Jukka Oikarinen <jukka.oikarinen@polar.com>
2020-12-02 10:32:50 +0200

    Demo app update: - gradle updated - update of dependencies to latest available version - migrate to AndroidX - cleanup of the code

--- demos/Android-Demos/PolarSDK-ECG-HR-Demo/app/src/main/java/com/polar/polarsdkecghrdemo/HRActivity.java
+++ demos/Android-Demos/PolarSDK-ECG-HR-Demo/app/src/main/java/com/polar/polarsdkecghrdemo/HRActivity.java
@@ -2,11 +2,13 @@ package com.polar.polarsdkecghrdemo;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.androidplot.xy.BoundaryMode;
 import com.androidplot.xy.StepMode;
@@ -17,31 +19,27 @@ import java.text.DecimalFormat;
 import java.util.List;
 import java.util.UUID;
 
-import io.reactivex.rxjava3.disposables.Disposable;
 import polar.com.sdk.api.PolarBleApi;
 import polar.com.sdk.api.PolarBleApiCallback;
 import polar.com.sdk.api.PolarBleApiDefaultImpl;
+import polar.com.sdk.api.errors.PolarInvalidArgument;
 import polar.com.sdk.api.model.PolarDeviceInfo;
 import polar.com.sdk.api.model.PolarHrData;
-import polar.com.sdk.api.errors.PolarInvalidArgument;
 
 public class HRActivity extends AppCompatActivity implements PlotterListener {
-
+    private static final String TAG = "HRActivity";
     private XYPlot plot;
     private TimePlotter plotter;
-
-    TextView textViewHR, textViewFW;
-    private String TAG = "Polar_HRActivity";
-    public PolarBleApi api;
-    private Disposable ecgDisposable = null;
-    private Context classContext = this;
-    private String DEVICE_ID;
+    private TextView textViewHR;
+    private TextView textViewFW;
+    private PolarBleApi api;
+    private final Context classContext = this;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hr);
-        DEVICE_ID = getIntent().getStringExtra("id");
+        String deviceId = getIntent().getStringExtra("id");
         textViewHR = findViewById(R.id.info2);
         textViewFW = findViewById(R.id.fw2);
 
@@ -58,56 +56,56 @@ public class HRActivity extends AppCompatActivity implements PlotterListener {
             }
 
             @Override
-            public void deviceConnected(PolarDeviceInfo s) {
+            public void deviceConnected(@NonNull PolarDeviceInfo s) {
                 Log.d(TAG, "Device connected " + s.deviceId);
                 Toast.makeText(classContext, R.string.connected,
                         Toast.LENGTH_SHORT).show();
             }
 
             @Override
-            public void deviceConnecting(PolarDeviceInfo polarDeviceInfo) {
+            public void deviceConnecting(@NonNull PolarDeviceInfo polarDeviceInfo) {
 
             }
 
             @Override
-            public void deviceDisconnected(PolarDeviceInfo s) {
+            public void deviceDisconnected(@NonNull PolarDeviceInfo s) {
                 Log.d(TAG, "Device disconnected " + s);
 
             }
 
             @Override
-            public void ecgFeatureReady(String s) {
+            public void ecgFeatureReady(@NonNull String s) {
                 Log.d(TAG, "ECG Feature ready " + s);
             }
 
             @Override
-            public void accelerometerFeatureReady(String s) {
+            public void accelerometerFeatureReady(@NonNull String s) {
                 Log.d(TAG, "ACC Feature ready " + s);
             }
 
             @Override
-            public void ppgFeatureReady(String s) {
+            public void ppgFeatureReady(@NonNull String s) {
                 Log.d(TAG, "PPG Feature ready " + s);
             }
 
             @Override
-            public void ppiFeatureReady(String s) {
+            public void ppiFeatureReady(@NonNull String s) {
                 Log.d(TAG, "PPI Feature ready " + s);
             }
 
             @Override
-            public void biozFeatureReady(String s) {
+            public void biozFeatureReady(@NonNull String s) {
 
             }
 
             @Override
-            public void hrFeatureReady(String s) {
+            public void hrFeatureReady(@NonNull String s) {
                 Log.d(TAG, "HR Feature ready " + s);
             }
 
             @Override
-            public void disInformationReceived(String s, UUID u, String s1) {
-                if( u.equals(UUID.fromString("00002a28-0000-1000-8000-00805f9b34fb"))) {
+            public void disInformationReceived(@NonNull String s, @NonNull UUID u, @NonNull String s1) {
+                if (u.equals(UUID.fromString("00002a28-0000-1000-8000-00805f9b34fb"))) {
                     String msg = "Firmware: " + s1.trim();
                     Log.d(TAG, "Firmware: " + s + " " + s1.trim());
                     textViewFW.append(msg + "\n");
@@ -115,7 +113,7 @@ public class HRActivity extends AppCompatActivity implements PlotterListener {
             }
 
             @Override
-            public void batteryLevelReceived(String s, int i) {
+            public void batteryLevelReceived(@NonNull String s, int i) {
                 String msg = "ID: " + s + "\nBattery level: " + i;
                 Log.d(TAG, "Battery level " + s + " " + i);
 //                Toast.makeText(classContext, msg, Toast.LENGTH_LONG).show();
@@ -123,11 +121,11 @@ public class HRActivity extends AppCompatActivity implements PlotterListener {
             }
 
             @Override
-            public void hrNotificationReceived(String s,
-                                               PolarHrData polarHrData) {
+            public void hrNotificationReceived(@NonNull String s,
+                                               @NonNull PolarHrData polarHrData) {
                 Log.d(TAG, "HR " + polarHrData.hr);
                 List<Integer> rrsMs = polarHrData.rrsMs;
-                String msg = String.valueOf(polarHrData.hr) + "\n";
+                String msg = polarHrData.hr + "\n";
                 for (int i : rrsMs) {
                     msg += i + ",";
                 }
@@ -139,25 +137,23 @@ public class HRActivity extends AppCompatActivity implements PlotterListener {
             }
 
             @Override
-            public void polarFtpFeatureReady(String s) {
+            public void polarFtpFeatureReady(@NonNull String s) {
                 Log.d(TAG, "Polar FTP ready " + s);
             }
         });
         try {
-            api.connectToDevice(DEVICE_ID);
-        } catch (PolarInvalidArgument a){
+            api.connectToDevice(deviceId);
+        } catch (PolarInvalidArgument a) {
             a.printStackTrace();
         }
 
-        plotter = new TimePlotter(this, "HR/RR");
+        plotter = new TimePlotter();
         plotter.setListener(this);
 
         plot.addSeries(plotter.getHrSeries(), plotter.getHrFormatter());
         plot.addSeries(plotter.getRrSeries(), plotter.getRrFormatter());
-        plot.setRangeBoundaries(50, 100,
-                BoundaryMode.AUTO);
-        plot.setDomainBoundaries(0, 360000,
-                BoundaryMode.AUTO);
+        plot.setRangeBoundaries(50, 100, BoundaryMode.AUTO);
+        plot.setDomainBoundaries(0, 360000, BoundaryMode.AUTO);
         // Left labels will increment by 10
         plot.setRangeStep(StepMode.INCREMENT_BY_VAL, 10);
         plot.setDomainStep(StepMode.INCREMENT_BY_VAL, 60000);
@@ -175,11 +171,6 @@ public class HRActivity extends AppCompatActivity implements PlotterListener {
     }
 
     public void update() {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                plot.redraw();
-            }
-        });
+        runOnUiThread(() -> plot.redraw());
     }
 }
