a83ac5ff67e164ea4f69014d79f66458e61026e2
Jukka Oikarinen <jukka.oikarinen@polar.com>
2021-01-13 08:55:55 +0200

    Minor: Build script to create output folder if not existing already

--- sources/Android/android-communications/scripts/build_sdk.sh
+++ sources/Android/android-communications/scripts/build_sdk.sh
@@ -6,5 +6,6 @@ rm -f polar-ble-sdk.aar
 cd ..
 ./gradlew clean
 ./gradlew assembleRelease
+mkdir -p SdkBuild
 cp build/outputs/aar/android-communications-release.aar SdkBuild/polar-ble-sdk.aar
 cp libs/polar-protobuf-release.aar SdkBuild/polar-protobuf-release.aar
