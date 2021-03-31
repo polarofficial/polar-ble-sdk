8cb292f66df1c85c577523c19ae2b51b542d8ea5
Jukka Oikarinen <jukka.oikarinen@polar.com>
2021-03-31 10:51:56 +0300

    iOS example uses the Pod dependency

---
+++ examples/example-ios/Pods/PolarBleSdk/polar-sdk-ios/PolarBleSdk.xcframework/ios-arm64_x86_64-simulator/PolarBleSdk.framework/Headers/PolarBleSdk.h
@@ -0,0 +1,12 @@
+/// Copyright © 2019 Polar Electro Oy. All rights reserved.
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PolarBleSdk.
+FOUNDATION_EXPORT double PolarBleSdkVersionNumber;
+
+//! Project version string for PolarBleSdk.
+FOUNDATION_EXPORT const unsigned char PolarBleSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PolarBleSdk/PublicHeader.h>
+
---
+++ examples/example-ios/Pods/Target Support Files/Pods-polarBleSdkTestApp/Pods-polarBleSdkTestApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_polarBleSdkTestApp : NSObject
+@end
+@implementation PodsDummy_Pods_polarBleSdkTestApp
+@end
