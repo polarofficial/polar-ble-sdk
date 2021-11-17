3d7122b1ad25fd51720f7de5499633c549c38aea
Jukka Oikarinen <jukka.oikarinen@polar.com>
2021-11-17 08:04:01 +0200

    Android Demo: update targetSDK from 29 to 31 and code cleanup.

---
+++ demos/Android-Demos/PolarSDK-ECG-HR-Demo/app/src/main/java/com/polar/polarsdkecghrdemo/MainActivity.kt
@@ -0,0 +1,144 @@
+package com.polar.polarsdkecghrdemo
+
+import android.Manifest
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.text.InputType
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "Polar_MainActivity"
+        private const val SHARED_PREFS_KEY = "polar_device_id"
+        private const val PERMISSION_REQUEST_CODE = 1
+    }
+
+    private lateinit var sharedPreferences: SharedPreferences
+    private val bluetoothOnActivityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+        if (result.resultCode != Activity.RESULT_OK) {
+            Log.w(TAG, "Bluetooth off")
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        sharedPreferences = getPreferences(MODE_PRIVATE)
+        val setIdButton: Button = findViewById(R.id.buttonSetID)
+        val ecgConnectButton: Button = findViewById(R.id.buttonConnectEcg)
+        val hrConnectButton: Button = findViewById(R.id.buttonConnectHr)
+        checkBT()
+
+        setIdButton.setOnClickListener { onClickChangeID(it) }
+        ecgConnectButton.setOnClickListener { onClickConnectEcg(it) }
+        hrConnectButton.setOnClickListener { onClickConnectHr(it) }
+    }
+
+    private fun onClickConnectEcg(view: View) {
+        checkBT()
+        val deviceId = sharedPreferences.getString(SHARED_PREFS_KEY, "")
+        if (deviceId == null || deviceId == "") {
+            showDialog(view)
+        } else {
+            showToast(getString(R.string.connecting) + " " + deviceId)
+            val intent = Intent(this, ECGActivity::class.java)
+            intent.putExtra("id", deviceId)
+            startActivity(intent)
+        }
+    }
+
+    private fun onClickConnectHr(view: View) {
+        checkBT()
+        val deviceId = sharedPreferences.getString(SHARED_PREFS_KEY, "")
+        if (deviceId == null || deviceId == "") {
+            showDialog(view)
+        } else {
+            showToast(getString(R.string.connecting) + " " + deviceId)
+            val intent = Intent(this, HRActivity::class.java)
+            intent.putExtra("id", deviceId)
+            startActivity(intent)
+        }
+    }
+
+    private fun onClickChangeID(view: View) {
+        showDialog(view)
+    }
+
+    private fun showDialog(view: View) {
+        val dialog = AlertDialog.Builder(this, R.style.PolarTheme)
+        dialog.setTitle("Enter your Polar device's ID")
+        val viewInflated = LayoutInflater.from(applicationContext).inflate(R.layout.device_id_dialog_layout, view.rootView as ViewGroup, false)
+        val input = viewInflated.findViewById<EditText>(R.id.input)
+        input.inputType = InputType.TYPE_CLASS_TEXT
+        dialog.setView(viewInflated)
+        dialog.setPositiveButton("OK") { _: DialogInterface?, _: Int ->
+            val deviceId = input.text.toString().uppercase()
+            val editor = sharedPreferences.edit()
+            editor.putString(SHARED_PREFS_KEY, deviceId)
+            editor.apply()
+        }
+        dialog.setNegativeButton("Cancel") { dialogInterface: DialogInterface, _: Int -> dialogInterface.cancel() }
+        dialog.show()
+    }
+
+    private fun checkBT() {
+        val btManager = applicationContext.getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
+        val bluetoothAdapter: BluetoothAdapter? = btManager.adapter
+        if (bluetoothAdapter == null) {
+            showToast("Device doesn't support Bluetooth")
+            return
+        }
+
+        if (!bluetoothAdapter.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            bluetoothOnActivityResultLauncher.launch(enableBtIntent)
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    requestPermissions(arrayOf(Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.BLUETOOTH_CONNECT), PERMISSION_REQUEST_CODE)
+                } else {
+                    requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_REQUEST_CODE)
+                }
+            }
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), PERMISSION_REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            for (index in 0..grantResults.lastIndex) {
+                if (grantResults[index] == PackageManager.PERMISSION_DENIED) {
+                    Log.w(TAG, "Needed permissions are missing")
+                    showToast("Needed permissions are missing")
+                    return
+                }
+            }
+            Log.d(TAG, "Needed permissions are granted")
+        }
+    }
+
+    private fun showToast(message: String) {
+        val toast = Toast.makeText(applicationContext, message, Toast.LENGTH_LONG)
+        toast.show()
+    }
+}
