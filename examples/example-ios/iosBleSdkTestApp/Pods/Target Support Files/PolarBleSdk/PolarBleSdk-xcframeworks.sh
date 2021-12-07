6a9896f4676e648c8e1441d67d830ce471bef821
Jukka Oikarinen <jukka.oikarinen@polar.com>
2021-12-07 13:50:34 +0200

    iOS example: updated Polar BLE SDK from version 3.2.4 to 3.2.5

--- examples/example-ios/iosBleSdkTestApp/Pods/Target Support Files/PolarBleSdk/PolarBleSdk-xcframeworks.sh
+++ examples/example-ios/iosBleSdkTestApp/Pods/Target Support Files/PolarBleSdk/PolarBleSdk-xcframeworks.sh
@@ -149,5 +149,5 @@ install_xcframework() {
   echo "Copied $source to $destination"
 }
 
-install_xcframework "${PODS_ROOT}/PolarBleSdk/polar-sdk-ios/PolarBleSdk.xcframework" "PolarBleSdk" "framework" "ios-arm64_x86_64-simulator" "ios-arm64"
+install_xcframework "${PODS_ROOT}/PolarBleSdk/polar-sdk-ios/PolarBleSdk.xcframework" "PolarBleSdk" "framework" "ios-arm64" "ios-arm64_x86_64-simulator"
 
