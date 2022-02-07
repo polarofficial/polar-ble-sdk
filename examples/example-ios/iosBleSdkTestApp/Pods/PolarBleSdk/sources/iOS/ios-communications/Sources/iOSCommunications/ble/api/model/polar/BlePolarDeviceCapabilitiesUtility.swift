3046f8e212fad6b7ab717da826d13444ee081072
Jukka Oikarinen <jukka.oikarinen@polar.com>
2022-02-07 11:46:31 +0200

    iOS Example: dependency to 3.2.7 updated

--- examples/example-ios/iosBleSdkTestApp/Pods/PolarBleSdk/sources/iOS/ios-communications/Sources/iOSCommunications/ble/api/model/polar/BlePolarDeviceCapabilitiesUtility.swift
+++ examples/example-ios/iosBleSdkTestApp/Pods/PolarBleSdk/sources/iOS/ios-communications/Sources/iOSCommunications/ble/api/model/polar/BlePolarDeviceCapabilitiesUtility.swift
@@ -2,7 +2,7 @@
 import Foundation
 
 open class BlePolarDeviceCapabilitiesUtility {
-    enum FileSystemType {
+    public enum FileSystemType {
         case unknownFileSystem
         case h10FileSystem
         case sagRfc2FileSystem
@@ -11,7 +11,7 @@ open class BlePolarDeviceCapabilitiesUtility {
     /// Get type of filesystem the device supports
     /// - Parameter deviceType:  device type
     /// - Returns: type of the file system supported or unknown file system type
-    static func fileSystemType(_ deviceType: String) -> FileSystemType {
+   public static func fileSystemType(_ deviceType: String) -> FileSystemType {
         switch deviceType.lowercased() {
         case "h10":
             return FileSystemType.h10FileSystem
@@ -26,7 +26,7 @@ open class BlePolarDeviceCapabilitiesUtility {
     /// Check if device is supporting recording start and stop over BLE
     /// - Parameter deviceType: device type
     /// - Returns: true if device supports recoding
-    static func isRecordingSupported(_ deviceType: String) -> Bool {
+    public static func isRecordingSupported(_ deviceType: String) -> Bool {
         return deviceType == "H10"
     }
 }
