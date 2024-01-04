bc7f2a17e0f0d071a1d806350d758660d89fa3fc
Samuli Määttä <samuli.maatta@polar.com>
2024-01-04 12:03:30 +0200

    iOS example uses SDK v5.5.0

--- sources/iOS/ios-communications/Sources/PolarBleSdk/sdk/api/model/SdkModeLed.swift
+++
@@ -1,9 +0,0 @@
-//  Copyright © 2023 Polar. All rights reserved.
-
-import Foundation
-
-public struct SdkModeLed {
-    public static let SDK_MODE_LEDS_FILENAME = "/SDKLED.BIN"
-    public static let LED_ANIMATION_DISABLE_BYTE: UInt8 = 0x00
-    public static let LED_ANIMATION_ENABLE_BYTE: UInt8 = 0x01
-}
