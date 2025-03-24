7485d1cadd5bda6a3420357f5ae4fcae207d4e79
Dominik Roszkowski <dominik@roszkowski.dev>
2025-03-24 10:47:47 +0100

    make fetchSession public

--- examples/example-ios/iosBleSdkTestApp/Pods/PolarBleSdk/sources/iOS/ios-communications/Sources/PolarBleSdk/sdk/impl/PolarBleApiImpl.swift
+++ examples/example-ios/iosBleSdkTestApp/Pods/PolarBleSdk/sources/iOS/ios-communications/Sources/PolarBleSdk/sdk/impl/PolarBleApiImpl.swift
@@ -207,7 +207,7 @@ import UIKit
         throw PolarErrors.deviceNotFound
     }
     
-    func fetchSession(_ identifier: String) throws -> BleDeviceSession? {
+    public func fetchSession(_ identifier: String) throws -> BleDeviceSession? {
         if identifier.matches("^([0-9a-fA-F]{8})(-[0-9a-fA-F]{4}){3}-([0-9a-fA-F]{12})") {
             return sessionByDeviceAddress(identifier)
         } else if identifier.matches("([0-9a-fA-F]){6,8}") {
